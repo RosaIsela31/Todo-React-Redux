520dc8847a672ffef0b1dbaca2dc1219d4efd3f5
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2019-12-11 14:35:02 -0600

    Updates

---
+++ precache-manifest.ffca9b252e49231e2c5218e288c5f8c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef00be5e266ffeeee512c51c972c85a1",
+    "url": "/RosaIsela31/Todo-React-Redux/index.html"
+  },
+  {
+    "revision": "83d7aa7d99ce79a378c2",
+    "url": "/RosaIsela31/Todo-React-Redux/static/css/main.61bb14ef.chunk.css"
+  },
+  {
+    "revision": "7355ec99cde833a13c22",
+    "url": "/RosaIsela31/Todo-React-Redux/static/js/2.f995c9d7.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/RosaIsela31/Todo-React-Redux/static/js/2.f995c9d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83d7aa7d99ce79a378c2",
+    "url": "/RosaIsela31/Todo-React-Redux/static/js/main.962930ed.chunk.js"
+  },
+  {
+    "revision": "1c5748f14789c88086c0",
+    "url": "/RosaIsela31/Todo-React-Redux/static/js/runtime-main.84457175.js"
+  }
+]);
